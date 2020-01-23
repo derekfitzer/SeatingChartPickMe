83e3911574ea96b1019921e565ac4465c0f3327c
Derek Fitzer <franzderek@gmail.com>
2020-01-23 08:18:05 -0500

    Initial Commit

---
+++ SeatingChartPickMe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SeatingChartPickMe
+//
+//  Created by Derek Fitzer on 1/23/20.
+//  Copyright © 2020 Derek Fitzer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
